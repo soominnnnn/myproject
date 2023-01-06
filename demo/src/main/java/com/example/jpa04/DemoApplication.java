4a55efa2c463209adc4c4196ec90b109b7997f8c
TJOEUN <TJOEUN@DESKTOP-R8FT4UV>
2023-01-06 10:03:34 +0900

    message

---
+++ demo/src/main/java/com/example/jpa04/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.jpa04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("github test");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
